1a4f9782d198b62d2abbe9bc55fb7fd2fa413173
Lorenzo Lucena Maguire <llucena@big19.seas.upenn.edu>
2023-02-03 14:05:17 -0500

    divider working

--- lab2a-div/lc4_divider.v
+++ lab2a-div/lc4_divider.v
@@ -11,27 +11,36 @@ module lc4_divider(input  wire [15:0] i_dividend,
                    output wire [15:0] o_quotient);
 
       /*** YOUR CODE HERE ***/
-      wire [15:0] curr_quotient;
-      wire [15:0] dividend_temp;
-      wire [15:0] curr_remainder;
+      // Holder bus for temporary results: 16 groups of 3 wires, 16 bits wide
+      wire [15:0] curr_remainder[16];
+      wire [15:0] curr_quotient[16];
+      wire [15:0] curr_dividend[16];
+      
+      // First pass
+      lc4_divider_one_iter d0(
+                        .i_dividend(i_dividend), 
+                        .i_divisor(i_divisor), 
+                        .i_remainder(16'b0), 
+                        .i_quotient(16'b0), 
+                        .o_dividend(curr_dividend[0]), 
+                        .o_remainder(curr_remainder[0]),
+                        .o_quotient(curr_quotient[0])
+                        );
 
-
-      lc4_divider_one_iter first(.i_dividend(i_dividend), .i_divisor(i_divisor), .i_remainder(16'b0),
-                              .i_quotient(16'b0), .o_dividend(dividend_temp[0]), .o_remainder(curr_remainder[0]),
-                              .o_quotient(curr_quotient[0]));
-
-      genvar count;
-      for (count = 1; count < 15; count= count+1) begin
-            lc4_divider_one_iter middle(.i_dividend(dividend_temp[count-1]), .i_divisor(i_divisor), .i_remainder(curr_remainder[count-1]),
-                              .i_quotient(curr_quotient[count-1]), .o_dividend(dividend_temp[count]), .o_remainder(curr_remainder[count]),
-                              .o_quotient(curr_quotient[count]));
+      genvar i;
+      for (i = 0; i < 15; i = i + 1) begin
+            lc4_divider_one_iter divider(
+                              .i_dividend(curr_dividend[i]), 
+                              .i_divisor(i_divisor), 
+                              .i_remainder(curr_remainder[i]), 
+                              .i_quotient(curr_quotient[i]), 
+                              .o_dividend(curr_dividend[i + 1]), 
+                              .o_remainder(curr_remainder[i + 1]),
+                              .o_quotient(curr_quotient[i+1])
+                              );
       end
-
-      lc4_divider_one_iter last(.i_dividend(dividend_temp[14]), .i_divisor(i_divisor), .i_remainder(curr_remainder[14]),
-                              .i_quotient(curr_quotient[14]), .o_dividend(dividend_temp[15]), .o_remainder(o_remainder),
-                              .o_quotient(o_quotient));
-
-
+      assign o_quotient = curr_quotient[15];
+      assign o_remainder = curr_remainder[15];
 endmodule // lc4_divider
 
 module lc4_divider_one_iter(input  wire [15:0] i_dividend,
@@ -43,29 +52,23 @@ module lc4_divider_one_iter(input  wire [15:0] i_dividend,
                             output wire [15:0] o_quotient);
 
       /*** YOUR CODE HERE ***/
+      wire [15:0] temp_remainder;
+      assign temp_remainder = i_dividend >> 15 | i_remainder << 1;
 
-      wire [15:0] curr_remainder;
-      assign curr_remainder = i_remainder << 1 | i_dividend >> 15;
-
-      assign o_dividend = i_dividend << 1;
-
+      // Calculate Remainder
       wire [15:0] subtract;
-      assign subtract = curr_remainder - i_divisor;
+      assign subtract = temp_remainder - i_divisor;
 
       wire [15:0] out_curr_remainder;
-      assign out_curr_remainder = (curr_remainder < i_divisor) ? curr_remainder : subtract;
+      assign out_curr_remainder = (temp_remainder >= i_divisor) ? subtract : temp_remainder;
 
-      wire [15:0] bits;
-      assign bits = (curr_remainder < i_divisor) ? 1'b0 : 1'b1;
-
-      wire [15:0] curr_quotient;
-      assign curr_quotient = i_quotient << 1;
-
+      // Calculate quotient
+      wire [15:0] shift_quotient;
+      assign shift_quotient = i_quotient << 1;
       wire [15:0] out_curr_quotient;
-      assign out_curr_quotient = curr_quotient | {15'b0, bits};
+      assign out_curr_quotient =  (temp_remainder >= i_divisor) ? shift_quotient | 1'b1 : shift_quotient; 
 
-      assign o_quotient = (i_divisor == 0) ? 16'b0 : out_curr_quotient;
-      assign o_remainder = (i_divisor == 0) ? 16'b0 : out_curr_remainder;
-
-
+      assign o_dividend = i_dividend << 1;
+      assign o_quotient = (i_divisor != 0) ? out_curr_quotient : 16'b0;
+      assign o_remainder = (i_divisor != 0) ? out_curr_remainder : 16'b0;
 endmodule
